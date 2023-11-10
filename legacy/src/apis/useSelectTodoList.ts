5bd7e340876d4fad3f89b6e8f04f7ec23263c384
minkyeongJ <jomk2221@naver.com>
2023-11-10 18:36:56 +0900

    Feat: 투두리스트 메인 페이지 구현

--- legacy/src/apis/useSelectTodoList.ts
+++ legacy/src/apis/useSelectTodoList.ts
@@ -1,31 +1,23 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
-type TodoInfo = {
-  _id: number;
-  title: string;
-  content: string;
-  done: boolean | true | false;
-  createdAt: string;
-  updatedAt: string;
-};
 
-type TodoList = {
-  ok: number;
-  items: TodoInfo[];
-  pagination: {
-    page: number;
-    limit: number;
-    total: number;
-    totalPages: number;
-  };
-};
+export default function useSelectTodoList() {
+  const [data, setData] = useState<TodoListResponse>(null);
+  const [error, setError] = useState(null);
 
-export default async function useSelectTodoList() {
-  try {
-    const response = await axios("http://localhost:33088/api/todolist");
-    return response.data as TodoList;
-  } catch (err) {
-    console.error(err);
-  }
+  useEffect(() => {
+    const fetchTodoList = async () => {
+      try {
+        const response = await axios("http://localhost:33088/api/todolist");
+        setData(response.data);
+      } catch (err) {
+        setError(err);
+        console.error(err);
+      }
+    };
+
+    fetchTodoList();
+  }, []); // 의존성 배열을 빈 배열로 설정하여 마운트될 때 한 번만 실행하도록 함
+
+  return { data, error };
 }
-
-export type { TodoInfo, TodoList };
--- src/features/list/List.tsx
+++ src/features/list/List.tsx
@@ -1,6 +1,8 @@
+import "./list.css";
 import useSelectTodoList from "services/useSelectTodoList";
 import ListCard from "./ListCard";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const List = () => {
   const [notDoneList, setNotDoneList] = useState<TodoItem[]>();
@@ -17,17 +19,25 @@ const List = () => {
   }, [todoList]);
 
   return (
-    <main>
-      <div>
+    <main className="list">
+      <select></select>
+      <Link to={`/regist`} className="regist-link">
+        <button className="regist-btn">등록</button>
+      </Link>
+      <section id="content-not-done">
+        <h2>TODO</h2>
+        <p className="todo-count">해야 할 일: {notDoneList?.length}</p>
         {notDoneList?.map((todo) => (
-          <ListCard todo={todo} />
+          <ListCard todo={todo} key={todo._id} />
         ))}
-      </div>
-      <div>
+      </section>
+      <section id="content-done">
+        <h2>DONE</h2>
+        <p className="done-count">완료 한 일: {doneList?.length}</p>
         {doneList?.map((todo) => (
-          <ListCard todo={todo} />
+          <ListCard todo={todo} key={todo._id} />
         ))}
-      </div>
+      </section>
     </main>
   );
 };
