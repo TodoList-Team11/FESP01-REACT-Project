de41dd319c09d3b609f791c59716fe1072a7a8af
minkyeongJ <jomk2221@naver.com>
2023-11-10 18:36:01 +0900

    Feat: 투두 리스트 item 카드 구현

--- src/features/list/ListCard.tsx
+++ src/features/list/ListCard.tsx
@@ -1,6 +1,18 @@
-import Draggable from "react-draggable";
+import "./listCard.css";
+// import Draggable from "react-draggable";
 import { Link } from "react-router-dom";
+import updateTodoInfoDone from "services/useUpdateTodoInfoDone";
 import handleDateForm from "utils/handleDateForm";
+
+export const handleCheckboxClick = ({
+  _id,
+  isDone,
+}: {
+  _id: number;
+  isDone: boolean;
+}) => {
+  updateTodoInfoDone({ _id, isDone });
+};
 
 /**
  * @param parentContent todo list 가 append 될 section
@@ -8,13 +20,20 @@ import handleDateForm from "utils/handleDateForm";
  */
 const ListCard = ({ todo }: { todo: TodoItem }) => {
   return (
-    <Draggable>
-      <div id={`${todo._id}`}>
-        <input type="checkbox" id={`${todo._id}`} checked={todo.done} />
-        <Link to={`/info/${todo._id}`}>{todo.title}</Link>
-        <span>`(${handleDateForm(todo.createdAt)})`</span>
-      </div>
-    </Draggable>
+    // <Draggable>
+    <div id={`${todo._id}`} className="list-card">
+      <input
+        type="checkbox"
+        id={`${todo._id}`}
+        defaultChecked={todo.done}
+        onClick={(e) =>
+          handleCheckboxClick({ _id: todo._id, isDone: todo.done })
+        }
+      />
+      <Link to={`/info/${todo._id}`}>{todo.title}</Link>
+      <span className="create-time">{handleDateForm(todo.createdAt)}</span>
+    </div>
+    // </Draggable>
   );
 };
 
