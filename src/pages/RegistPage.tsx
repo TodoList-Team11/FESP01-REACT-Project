573abee7d2e12e99ba04a250155990c0a8e7826c
Hyungeol Lee <ekfrhkquf12@naver.com>
2023-11-10 14:48:48 +0900

    Feat: RegistPage 초기 포매팅

--- src/pages/RegistPage.tsx
+++ src/pages/RegistPage.tsx
@@ -1,5 +1,32 @@
+import React from 'react'
+import {useRef} from 'react';
+
 const RegistPage = () => {
-  return <h1>register</h1>;
+  const title = useRef<HTMLInputElement>(null);
+  const content = useRef<HTMLTextAreaElement>(null);
+  // const content = useRef('')
+
+  return (
+  <>
+    <h1>TODO Register</h1>
+    <form>
+      <div>할 일</div>
+      <input 
+        placeholder="할 일을 입력하세요"
+        type = "text"
+        ref = {title}
+      />
+      <div>상세내용</div>
+      <textarea 
+        placeholder="상세 내용을 입력하세요"        
+        ref = {content}
+      />
+      <div> 
+        <button>등록</button>
+        <button>취소</button>
+      </div>
+    </form>
+  </>);
 };
 
 export default RegistPage;
