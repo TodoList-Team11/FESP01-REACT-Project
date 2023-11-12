3ae33c38f6346bd7073fb4d262cc05f011288d06
minkyeongJ <jomk2221@naver.com>
2023-11-12 23:47:08 +0900

    Feat: Drag&Drop 기능 구현

--- src/features/list/List.tsx
+++ src/features/list/List.tsx
@@ -1,8 +1,9 @@
 import "./list.css";
 import useSelectTodoList from "services/useSelectTodoList";
 import ListCard from "./ListCard";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import updateTodoInfoDone from "services/useUpdateTodoInfoDone";
 
 const List = () => {
   const [notDoneList, setNotDoneList] = useState<TodoItem[]>([]);
@@ -18,13 +19,45 @@ const List = () => {
     );
   }, [todoList]);
 
+  //Drag & Drop
+  const draggingItemId = useRef<string>();
+  type DropTodo = {
+    e: React.DragEvent;
+    isDone: boolean;
+  };
+  /**
+   * 드롭 이벤트 발생 시 done 상태변경 값을 서버로 전송
+   * @param e dropdown event
+   * @param isDone done true/false
+   */
+  const onDropTodo = async ({ e, isDone }: DropTodo) => {
+    e.preventDefault();
+    if (e !== null) {
+      const selectedTodo = e.dataTransfer!.getData("todo");
+      const todo = JSON.parse(selectedTodo);
+      await updateTodoInfoDone({ _id: parseInt(todo._id), isDone });
+      console.log("todo>>>", todo);
+      if (todo.done) {
+        setNotDoneList((todos) => [...todos, { ...todo, done: false }]);
+        setDoneList((todos) => todos.filter((el) => el._id !== todo._id));
+      } else if (!todo.done) {
+        setDoneList((todos) => [...todos, { ...todo, done: true }]);
+        setNotDoneList((todos) => todos.filter((el) => el._id !== todo._id));
+      }
+    }
+  };
+
   return (
     <main className="list">
       <select></select>
       <Link to={`/regist`} className="regist-link">
         <button className="regist-btn">등록</button>
       </Link>
-      <section id="content-not-done">
+      <section
+        id="content-not-done"
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={(e: React.DragEvent) => onDropTodo({ e: e, isDone: false })}
+      >
         <h2>TODO</h2>
         <p className="todo-count">해야 할 일: {notDoneList?.length}</p>
         {notDoneList?.map((todo) => (
@@ -36,7 +69,11 @@ const List = () => {
           />
         ))}
       </section>
-      <section id="content-done">
+      <section
+        id="content-done"
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={(e: React.DragEvent) => onDropTodo({ e: e, isDone: true })}
+      >
         <h2>DONE</h2>
         <p className="done-count">완료 한 일: {doneList?.length}</p>
         {doneList?.map((todo) => (
--- src/services/useUpdateTodoInfoDone.ts
+++ src/services/useUpdateTodoInfoDone.ts
@@ -8,7 +8,7 @@ interface Props {
 export default async function updateTodoInfoDone({ _id, isDone }: Props) {
   try {
     await axios.patch(`http://localhost:33088/api/todolist/${_id}`, {
-      done: !isDone,
+      done: isDone,
     });
   } catch (err) {
     console.log(err);
